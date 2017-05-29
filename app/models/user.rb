b22bee91994b013003a25d288762f3512857f923
m47h <matekstar@o2.pl>
2017-05-29 14:41:00 +0200

    Create Transfers scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
    has_secure_password
+   has_many :transfers
    
    validates_presence_of :email, :password, :first_name, :last_name, :dob, :address_line_1
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529120058) do
+ActiveRecord::Schema.define(version: 20170529122636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name",      limit: 20, null: false
-    t.string   "last_name",       limit: 20, null: false
-    t.string   "address_line_1",  limit: 50, null: false
-    t.date     "dob",                        null: false
-    t.string   "email",                      null: false
-    t.string   "password_digest",            null: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+  create_table "transfers", force: :cascade do |t|
+    t.string "account_number_from", null: false
+    t.string "account_number_to", null: false
+    t.integer "amount_pennies", null: false
+    t.string "country_code_from", null: false
+    t.string "country_code_to", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "first_name", limit: 20, null: false
+    t.string "last_name", limit: 20, null: false
+    t.string "address_line_1", limit: 50, null: false
+    t.date "dob", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
